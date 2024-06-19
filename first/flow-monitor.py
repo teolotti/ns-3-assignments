39eb53e63246e027bc796dab09443b3fe73f9795
teolotti <matteo.lotti3@edu.unifi.it>
2024-06-19 17:16:58 +0200

    latex table and added IPs

--- first/flow-monitor.py
+++ first/flow-monitor.py
@@ -8,6 +8,8 @@ def parse_flow_monitor_to_table(filename):
     data = []
 
     flowStats = root.find(".//FlowStats")
+
+    ipv4 = root.find(".//Ipv4FlowClassifier")
     
     for flow in flowStats.findall(".//Flow"):
         flow_id = flow.attrib['flowId']
@@ -21,23 +23,31 @@ def parse_flow_monitor_to_table(filename):
         time_last_rx_packet = float(flow.attrib['timeLastRxPacket'].split(' ')[0].replace('ns', ''))
         duration = time_last_rx_packet - time_first_tx_packet
         
-        rtt = delay_sum / rx_packets if rx_packets > 0 else 0
-        throughput = (rx_bytes * 8) / duration if duration > 0 else 0
+        rtt = (delay_sum / rx_packets) * 1e-6 if rx_packets > 0 else 0
+        throughput = (rx_bytes * 8 * 1e9) / duration if duration > 0 else 0
 
         data.append({
             "Flow ID": flow_id,
             "Tx Packets": tx_packets,
             "Rx Packets": rx_packets,
             "Lost Packets": lost_packets,
-            "RTT (s)": rtt,
+            "RTT (ms)": rtt,
             "Throughput (bps)": throughput
         })
 
+    for flow in ipv4.findall(".//Flow"):
+        source_ip = flow.attrib['sourceAddress']
+        destination_ip = flow.attrib['destinationAddress']
+        for d in data:
+            if d["Flow ID"] == flow.attrib['flowId']:
+                d["Source IP"] = source_ip
+                d["Destination IP"] = destination_ip
+        
     df = pd.DataFrame(data)
     return df
 
 if __name__ == "__main__":
-    filename = "/home/matteo/ns-3-dev/scratch/ns-3-assignments/first/first-ass-tcp.xml"
+    filename = "/home/matteo/ns-3-dev/scratch/ns-3-first-assignment/first/first-ass-tcp.xml"
     df = parse_flow_monitor_to_table(filename)
     print(df)
-    df.to_csv("flow_monitor_results.csv", index=False)
+    df.to_latex('/home/matteo/ns-3-dev/scratch/ns-3-first-assignment/first/data.tex', index=False)
