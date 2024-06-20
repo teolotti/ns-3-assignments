0937138906194aa7f7907c173d087b2fffda1213
teolotti <matteo.lotti3@edu.unifi.it>
2024-06-20 09:32:46 +0200

    fixed script and last files

--- first-assignment-tcp.cc
+++ first-assignment-tcp.cc
@@ -13,41 +13,43 @@ using namespace ns3;
 
 std::ofstream cwndFile;
 
-void CwndChange1 (uint32_t oldCwnd, uint32_t newCwnd)
+void CwndChange1(uint32_t oldCwnd, uint32_t newCwnd)
 {
   cwndFile << "1" << "\t" << Simulator::Now().GetSeconds() << "\t" << newCwnd << std::endl;
   std::cout << "1" << "\t" << Simulator::Now().GetSeconds() << "\t" << newCwnd << std::endl;
 }
 
-void CwndChange2 (uint32_t oldCwnd, uint32_t newCwnd)
+void CwndChange2(uint32_t oldCwnd, uint32_t newCwnd)
 {
   cwndFile << "2" << "\t" << Simulator::Now().GetSeconds() << "\t" << newCwnd << std::endl;
   std::cout << "2" << "\t" << Simulator::Now().GetSeconds() << "\t" << newCwnd << std::endl;
 }
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  Time::SetResolution (Time::NS);
-  //LogComponentEnable ("BulkSendApplication", LOG_LEVEL_INFO);
-  //LogComponentEnable ("PacketSink", LOG_LEVEL_INFO);
+  Time::SetResolution(Time::NS);
+
+  //LogComponentEnable("BulkSendApplication", LOG_LEVEL_INFO);
+  //LogComponentEnable("PacketSink", LOG_LEVEL_INFO);
   
   Config::SetDefault("ns3::TcpL4Protocol::SocketType", StringValue("ns3::TcpNewReno"));
   Config::SetDefault("ns3::TcpSocket::InitialCwnd", UintegerValue(1));
   Config::SetDefault("ns3::TcpL4Protocol::RecoveryType",
                        TypeIdValue(TypeId::LookupByName("ns3::TcpClassicRecovery")));
-  // Create 4 nodes
+
+  //Create 4 nodes
   NodeContainer nodes;
-  nodes.Create (4);
+  nodes.Create(4);
 
-  // Create point-to-point links
+  //Create point-to-point links
   PointToPointHelper pointToPoint;
-  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+  pointToPoint.SetDeviceAttribute("DataRate", StringValue ("5Mbps"));
+  pointToPoint.SetChannelAttribute("Delay", StringValue ("2ms"));
 
-  // Install devices on nodes
+  //Install devices on nodes
   NetDeviceContainer devices01, devices23;
-  devices01 = pointToPoint.Install (nodes.Get(0), nodes.Get(1));
-  devices23 = pointToPoint.Install (nodes.Get(2), nodes.Get(3));
+  devices01 = pointToPoint.Install(nodes.Get(0), nodes.Get(1));
+  devices23 = pointToPoint.Install(nodes.Get(2), nodes.Get(3));
 
   // Set error model, introduce a small error rate
   Ptr<RateErrorModel> em = CreateObject<RateErrorModel>();
@@ -57,88 +59,82 @@ int main (int argc, char *argv[])
 
   // Install Internet Stack
   InternetStackHelper stack;
-  stack.Install (nodes);
+  stack.Install(nodes);
 
   // Assign IP addresses
   Ipv4AddressHelper address;
-  address.SetBase ("10.1.1.0", "255.255.255.0");
-  Ipv4InterfaceContainer interfaces01 = address.Assign (devices01);
+  address.SetBase("10.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer interfaces01 = address.Assign(devices01);
 
-  address.SetBase ("10.1.2.0", "255.255.255.0");
-  Ipv4InterfaceContainer interfaces23 = address.Assign (devices23);
+  address.SetBase("10.1.2.0", "255.255.255.0");
+  Ipv4InterfaceContainer interfaces23 = address.Assign(devices23);
 
   // Get the IP addresses of the nodes
-  Ipv4Address ipNode1 = interfaces01.GetAddress (1);
-  Ipv4Address ipNode3 = interfaces23.GetAddress (1);
+  Ipv4Address ipNode1 = interfaces01.GetAddress(1);
+  Ipv4Address ipNode3 = interfaces23.GetAddress(1);
 
   // Configure TCP applications on nodes 0 and 1
+
   uint16_t port = 9;  // Port number for the server
 
   //Create a TCP socket
-  Ptr<Socket> TcpSocket1 = Socket::CreateSocket (nodes.Get (0), TcpSocketFactory::GetTypeId ());
-  TcpSocket1->TraceConnectWithoutContext ("CongestionWindow", MakeCallback (&CwndChange1));
+  Ptr<Socket> TcpSocket1 = Socket::CreateSocket(nodes.Get(0), TcpSocketFactory::GetTypeId());
+  TcpSocket1->TraceConnectWithoutContext("CongestionWindow", MakeCallback(&CwndChange1));
 
-  Address sinkAddress1 (InetSocketAddress (ipNode1, port));
+  Address sinkAddress1(InetSocketAddress(ipNode1, port));
 
-  Ptr<TutorialApp> app1 = CreateObject<TutorialApp> ();
-  app1->Setup (TcpSocket1, sinkAddress1, 1040, 10000, DataRate ("1Mbps"));
-  nodes.Get (0)->AddApplication (app1);
-  app1->SetStartTime (Seconds (1.0));
-  app1->SetStopTime (Seconds (10.0));
+  Ptr<TutorialApp> app1 = CreateObject<TutorialApp>();
+  app1->Setup(TcpSocket1, sinkAddress1, 1040, 10000, DataRate("1Mbps"));
+  nodes.Get(0)->AddApplication(app1);
+  app1->SetStartTime(Seconds(1.0));
+  app1->SetStopTime(Seconds(10.0));
 
   // Create a PacketSinkApplication and install it on node 1, listening only on the specific IP and port
-  PacketSinkHelper sink ("ns3::TcpSocketFactory", InetSocketAddress (ipNode1, port));
-  ApplicationContainer sinkApps = sink.Install (nodes.Get (1));
-  sinkApps.Start (Seconds (1.0));
-  sinkApps.Stop (Seconds (10.0));
+  PacketSinkHelper sink("ns3::TcpSocketFactory", InetSocketAddress(ipNode1, port));
+  ApplicationContainer sinkApps = sink.Install(nodes.Get(1));
+  sinkApps.Start(Seconds(1.0));
+  sinkApps.Stop(Seconds(10.0));
 
   // Configure TCP applications on nodes 2 and 3
+
   port = 10;  // Different port number for the second server
 
-  
   // Create a TCP socket
-  Ptr<Socket> TcpSocket2 = Socket::CreateSocket (nodes.Get (2), TcpSocketFactory::GetTypeId ());
-  TcpSocket2->TraceConnectWithoutContext ("CongestionWindow", MakeCallback (&CwndChange2));
+  Ptr<Socket> TcpSocket2 = Socket::CreateSocket(nodes.Get(2), TcpSocketFactory::GetTypeId());
+  TcpSocket2->TraceConnectWithoutContext("CongestionWindow", MakeCallback(&CwndChange2));
 
-  Address sinkAddress2 (InetSocketAddress (ipNode3, port));
+  Address sinkAddress2(InetSocketAddress(ipNode3, port));
 
-  Ptr<TutorialApp> app2 = CreateObject<TutorialApp> ();
-  app2->Setup (TcpSocket2, sinkAddress2, 1040, 10000, DataRate ("1Mbps"));
-  nodes.Get (2)->AddApplication (app2);
-  app2->SetStartTime (Seconds (1.0));
-  app2->SetStopTime (Seconds (10.0));
+  Ptr<TutorialApp> app2 = CreateObject<TutorialApp>();
+  app2->Setup(TcpSocket2, sinkAddress2, 1040, 10000, DataRate("1Mbps"));
+  nodes.Get(2)->AddApplication(app2);
+  app2->SetStartTime(Seconds(1.0));
+  app2->SetStopTime(Seconds(10.0));
 
   // Create a PacketSinkApplication and install it on node 3, listening only on the specific IP and port
-  PacketSinkHelper sink2 ("ns3::TcpSocketFactory", InetSocketAddress (ipNode3, port));
-  ApplicationContainer sinkApps2 = sink2.Install (nodes.Get (3));
-  sinkApps2.Start (Seconds (1.0));
-  sinkApps2.Stop (Seconds (10.0));
+  PacketSinkHelper sink2("ns3::TcpSocketFactory", InetSocketAddress(ipNode3, port));
+  ApplicationContainer sinkApps2 = sink2.Install(nodes.Get(3));
+  sinkApps2.Start(Seconds(1.0));
+  sinkApps2.Stop(Seconds(10.0));
 
-  cwndFile.open("scratch/first/first-assignment-tcp-cwnd.txt");
-
-
-  // Trace RxDrop on the two devices
-  //devices01.Get (1)->TraceConnectWithoutContext ("PhyRxDrop", MakeCallback (&RxDrop));
-  //devices23.Get (1)->TraceConnectWithoutContext ("PhyRxDrop", MakeCallback (&RxDrop));  
+  cwndFile.open("scratch/ns-3-first-assignment/first/first-assignment-tcp-cwnd.txt"); 
 
   // Flow monitor
   FlowMonitorHelper flowmon;
   Ptr<FlowMonitor> monitor = flowmon.InstallAll();
 
-  pointToPoint.EnablePcapAll ("scratch/ns-3-first-assignment/first/first-assignment-tcp");
+  //pointToPoint.EnablePcapAll("scratch/ns-3-first-assignment/first/first-assignment-tcp");
 
-  Simulator::Stop (Seconds (11.0));
-  Simulator::Run ();
+  Simulator::Stop(Seconds(11.0));
+  Simulator::Run();
 
   cwndFile.close();
 
-  monitor->CheckForLostPackets ();
+  monitor->CheckForLostPackets();
 
   // Specify the path to save the file
-  monitor->SerializeToXmlFile("scratch/first/first-ass-tcp.xml", true, true);
+  monitor->SerializeToXmlFile("scratch/ns-3-first-assignment/first/first-ass-tcp.xml", true, true);
 
-  //cwnd, se buffer è sempre pieno, varianza del throughput
-
-  Simulator::Destroy ();
+  Simulator::Destroy();
   return 0;
 }
